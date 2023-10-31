c1f8cb94661e041df0dcf736613d5aa445ec368d
Mahmoud Ghanem <moghanem345@gmail.com>
2023-10-31 22:25:32 +0200

    filter the tours + order them + validate the request

--- app/Http/Controllers/Api/V1/TourController.php
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -8,11 +8,41 @@ use Illuminate\Http\Request;
 use App\Models\Travel;
 use App\Http\Resources\TourResource;
 
+use Illuminate\Validation\Rule;
+
+
 class TourController extends Controller
 {
-    public function index(Travel $travel)
+    public function index(Travel $travel, Request $request)
     {
+        $request->validate([
+            'priceFrom' => 'nullable|numeric|min:0',
+            'priceTo' => 'nullable|numeric|min:0',
+            'dateFrom' => 'nullable|date',
+            'dateTo' => 'nullable|date',
+            'sortOrder' => Rule::in(['asc', 'desc']),
+            'sortBy' => Rule::in(['price']),
+        ], [
+            'sortBy' => 'The sort field must be one of the following types: price',
+            'sortOrder' => 'The sort order must be one of the following types: asc, desc'
+        ]);
+
         $tours = $travel->tours()
+            ->when($request->priceFrom, function ($query) use ($request) {
+                $query->where('price', '>=', $request->priceFrom);
+            })
+            ->when($request->priceFrom, function ($query) use ($request) {
+                $query->where('price', '>=', $request->priceFrom);
+            })
+            ->when($request->dateFrom, function ($query) use ($request) {
+                $query->where('starting_date', '>=', $request->dateFrom);
+            })
+            ->when($request->dateTo, function ($query) use ($request) {
+                $query->where('starting_date', '>=', $request->dateTo);
+            })
+            ->when($request->sortBy || $request->sortOrder, function ($query) use ($request) {
+                $query->orderBy($request->sortBy, $request->sortOrder ?? 'asc');
+            })
             ->orderBy('starting_date', 'asc')
             ->paginate();
 
