432373497591cd052ec746452e436129ce663b6a
Mahmoud Ghanem <moghanem345@gmail.com>
2023-11-01 00:56:07 +0200

    create user command to create a new user

--- app/Console/Commands/CreateUserCommand.php
+++ app/Console/Commands/CreateUserCommand.php
@@ -3,6 +3,11 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Models\User;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
 
 class CreateUserCommand extends Command
 {
@@ -11,20 +16,40 @@ class CreateUserCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'app:create-user-command';
+    protected $signature = 'users:create';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Creates a new user';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        $user['name'] = $this->ask('What is your name?');
+        $user['email'] = $this->ask('What is your email?');
+        $user['password'] = HASH::make($this->secret('What is the password?'));
+        $roleName = $this->choice('What is your role?', ['admin', 'editor'], 1);
+        $role = \App\Models\Role::where('name', $roleName)->first();
+        if(! $role) {
+            $this->error('Role does not exist');
+            return -1;
+        }
+
+        DB::transaction(function() use ($user, $role) {
+            $user = User::create($user);
+            $user->roles()->attach($role->id);
+        });
+
+
+
+        $this->info('User created successfully.');
+
+        return 0;
+
     }
 }
